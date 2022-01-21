e6ced666d37e3b7700f1aa8229cb4174dae55e4b
Melissa Guachun <1212malka@gmail.com>
2022-01-20 23:17:39 -0500

    initial fetch accomplished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [dogs, setDogs] = useState([]);
+
+
+  useEffect(() => {
+    const url = "https://dog.ceo/api/breeds/image/random";
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json.message);
+        setDogs(json.message);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <img src={dogs} />
+    </>
+   
   );
 }
 
