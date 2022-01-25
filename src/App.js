f3442a84a9878414b2d7d61843607853b49f31d2
Melissa Guachun <1212malka@gmail.com>
2022-01-24 19:23:41 -0500

    imported boostrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import './App.css';
+import {Card, Button} from 'react-bootstrap';
 
 const App = () => {
   const [dogs, setDogs] = useState([]);
@@ -21,12 +23,24 @@ const App = () => {
     fetchData();
   }, []);
   
+  function refreshPage() {
+    window.location.reload(false);
+  }
    
   return (
-    <>
-    <h1>LOOK AT THESE BOIS</h1>
-    <img src={dogs} />
-    </>
+    // <>
+    // <h1>LOOK AT THESE BOIS</h1>
+    // <img alt="a random dog" src={dogs}/>
+    // </>
+    <Card className="text-center">
+    <Card.Header>Having a bad day?</Card.Header>
+    <Card.Body>
+      <Card.Title>Take a gander at these good bois!</Card.Title>
+      <img alt="a random dog" src={dogs}/>
+    </Card.Body>
+    <Button variant="primary" onClick={refreshPage}>Show Me Another One! </Button>
+    <Card.Footer className="text-muted">2 days ago</Card.Footer>
+  </Card>
    
   );
 }
