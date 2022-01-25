0ee774dd5546f81facd3b10b71f659203338472c
Melissa Guachun <1212malka@gmail.com>
2022-01-24 19:39:12 -0500

    import button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
-import {Card, Button} from 'react-bootstrap';
+import { Card, Button, ButtonGroup } from 'react-bootstrap';
+
 
 const App = () => {
   const [dogs, setDogs] = useState([]);
@@ -22,26 +23,28 @@ const App = () => {
 
     fetchData();
   }, []);
-  
+
   function refreshPage() {
     window.location.reload(false);
   }
-   
+
   return (
-    // <>
-    // <h1>LOOK AT THESE BOIS</h1>
-    // <img alt="a random dog" src={dogs}/>
-    // </>
-    <Card className="text-center">
-    <Card.Header>Having a bad day?</Card.Header>
-    <Card.Body>
-      <Card.Title>Take a gander at these good bois!</Card.Title>
-      <img alt="a random dog" src={dogs}/>
-    </Card.Body>
-    <Button variant="primary" onClick={refreshPage}>Show Me Another One! </Button>
-    <Card.Footer className="text-muted">2 days ago</Card.Footer>
-  </Card>
-   
+  
+    <div>
+      <h2>Having a bad day?</h2>
+      <h3>Take a gander at these good bois!</h3>
+      <img alt="a random dog" src={dogs} />
+      <div>
+        <>
+          <ButtonGroup size="lg" className="mb-2">
+            <Button onClick={refreshPage}>Show Me Another One! </Button>
+          </ButtonGroup>
+        </>
+      </div>
+    </div>
+
+
+
   );
 }
 
